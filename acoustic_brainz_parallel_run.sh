e0fa7447f80c9ecf8ab9153bfbdfdfa353a06adf
JDiChiacchio <jacob_dichiacchio@brown.edu>
2021-04-08 19:51:17 -0400

    better progress checking

--- acoustic_brainz_parallel_run.sh
+++ acoustic_brainz_parallel_run.sh
@@ -2,5 +2,6 @@
 
 nums="0 1 2 3 4 5 6 7 8 9 a b c d e f"
 for val in $nums; do
-    nohup python acoustic_brainz_db_constructor.py "$val" >> ../acousticbrnz_out &
+    echo > ../acousticbrnz_out/"$val"
+    nohup python acoustic_brainz_db_constructor.py "$val" >> ../acousticbrnz_out/"$val" &
 done
