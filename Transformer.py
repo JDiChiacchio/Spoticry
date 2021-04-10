974b4621a4332d193277ec8049fc802ffa6b3a16
Gus VanNewkirk <gusvannewkirk@Guss-MBP.fios-router.home>
2021-04-09 23:32:18 -0400

    perm

--- Transformer.py
+++ Transformer.py
@@ -46,7 +46,7 @@ class Transformer(tf.Module):
         q = tf.linalg.matmul(embedded, self.W_q)
         v = tf.linalg.matmul(embedded, self.W_v)
 
-        z = tf.matmul(tf.nn.softmax(tf.matmul(q,k.T)/(self.kqv_size **(.5))), v)
+        z = tf.matmul(tf.nn.softmax(tf.matmul(q,tf.transpose(k, perm=[0,2,1]))/(self.kqv_size **(.5))), v)
 
         return tf.matmul(z, self.dense) + self.bias
 
