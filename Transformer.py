f209f0dcc14d64b4145084074e6ee02f04e200dc
Gus VanNewkirk <gusvannewkirk@Guss-MBP.fios-router.home>
2021-04-09 20:00:12 -0400

    transformer

--- Transformer.py
+++ Transformer.py
@@ -1,28 +1,126 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
-window_size = 40 #????
-embedding_size = 50
-kqv_size = 64 
-
-#Transformer
-
 import tensorflow as tf
+from preprocess import *
+from comet_ml import Experiment
 
-embedding_dict = {}
+hyper_params = {
+     "batch_size": 16,
+     "num_epochs": 1,
+     "learning_rate": .001,
+     "window_size": 5, #might have to be tiny
+     "embedding_size": 50,
+     "kqv_size": 64
+ }
 
-#loop through all matches in db and add their vectors as tensors to the dict and make them untrainable
+class Transformer(tf.Module):
+
+    def __init__(self, embedding_table):
+        super().__init__()
+
+        self.batch_size = hyper_params["batch_size"]
+        self.num_epochs = hyper_params["num_epochs"]
+        self.lr = hyper_params["learning_rate"]
+        self.window_size = hyper_params["window_size"]
+        self.embedding_size = hyper_params["embedding_size"]
+        self.kqv_size = hyper_params["kqv_size"]
+
+        self.W_k = tf.Variable(tf.random.normal(shape = (embedding_size, kqv_size)))
+        self.W_q = tf.Variable(tf.random.normal(shape = (embedding_size, kqv_size)))
+        self.W_v = tf.Variable(tf.random.normal(shape = (embedding_size, kqv_size)))
+
+        self.dense = tf.Variable(tf.random.normal(shape = (kqv_size, embedding_size)))
+        self.bias = tf.Variable(tf.random.normal(shape = (embedding_size)))
+
+        self.embeddings = tf.tensor(embedding_table)
+
+    def get_embedding(indices):
+        return tf.gather(self.embeddings,indices)
+
+    def forward(self, input):
+        #takes in batches of size batch_size x window_size
+
+        embedded = self.get_embedding(input)
+
+        k = tf.linalg.matmul(embedded, self.W_k)
+        q = tf.linalg.matmul(embedded, self.W_q)
+        v = tf.linalg.matmul(embedded, self.W_v)
+
+        z = tf.matmul(tf.nn.softmax(tf.matmul(q,k.T)/(self.kqv_size **(.5))), v)
+
+        return tf.matmul(z, self.dense) + self.bias
 
 
-W_q = tf.Variable(tf.random.normal(shape = (embedding_size, kqv_size)))
-W_k = tf.Variable(tf.random.normal(shape = (embedding_size, kqv_size)))
-W_v = tf.Variable(tf.random.normal(shape = (embedding_size, kqv_size)))
+def train(model, inputs, labels):
 
+    optimizer = tf.keras.optimizers.Adam(model.lr)
+    loss_fn = tf.keras.losses.MSE()
 
+    #shuffling
+    seed = np.random.randint(1,1000)
+    inputs = tf.random.shuffle(data, seed)
+    labels = tf.random.shuffle(data, seed)
+    num_batches = inputs.shape[0]//model.batch_size
 
-# making training data
-#remove 1 song from sequence of songs and hold that out as a label for each
-#also could mask a certain percentage like BERT
+    for epoch in self.num_epochs:
+        for batch in range(num_batches):
+            with tf.GradientTape() as tape:
 
+                batch_inputs = inputs[batch*model.batch_size:(batch+1)*model.batch_size]
+                batch_labels = labels[batch*model.batch_size:(batch+1)*model.batch_size]
+
+                out = model.forward(batch_inputs)
+                loss = loss_fn(model.get_embedding(batch_labels), out)
+
+                grads = tape.gradient(loss, model.trainable_weights)
+                optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+                experiment.log_metric("loss",loss,step= epoch*num_batches + batch)
+
+def test(model, inputs, labels):
+
+    loss_fn = tf.keras.losses.MSE()
+    model = model.evaluate()
+
+    total_model_loss = 0.0
+    total_avg_loss = 0.0
+
+    num_batches = inputs.shape[0]//model.batch_size
+    for batch in range(num_batches):
+
+        batch_inputs = inputs[batch*model.batch_size:(batch+1)*model.batch_size]
+        batch_labels = labels[batch*model.batch_size:(batch+1)*model.batch_size]
+        batch_labels = model.get_embedding(batch_labels)
+
+        model_out = model.forward(batch_inputs)
+        avg_out = tf.math.reduce_mean(batch_inputs, axis=1)
+
+        model_loss = loss_fn(batch_labels, model_out)
+        avg_loss = loss_fn(batch_labels, avg_out)
+
+        total_model_loss += model_loss
+        total_avg_loss += avg_loss
+
+    print("model loss:", total_model_loss)
+    print("avg loss:", total_avg_loss)
+    experiment.log_metric("model test loss", total_model_loss)
+    experiment.log_metric("avg test loss", total_avg_loss)
+
+if __name__ == "__main__":
+
+    experiment = Experiment(
+        api_key="2jaNx8WWN5smqSk7XGawlXOFF",
+        project_name="spoticry",
+        workspace="gvannewk",
+    )
+
+    experiment.log_parameters(hyper_params)
+
+    train_inputs, train_labels, test_inputs, test_labels, embedding_table, id_dict = preprocess()
+
+    model = Transformer(embedding_table)
+    train(model, train_inputs, train_labels)
+    #save model
+    tf.saved_model.save(model, '/transformer_model')
+
+    test(model, test_inputs, test_labels)
---
+++ preprocess.py
@@ -0,0 +1,40 @@
+
+def preprocess(data):
+    next_id = 0
+    id_dict = {} #music_brainz_id to num
+    embedding_table = []
+    inputs = []
+    labels = []
+
+    #loop through data vectorizing and padding
+    for user in data:
+        song_list = []
+        for song in user:
+            id = song[0]
+            vec = song[2]
+            if not id_dict.get(id)!= None:
+                id_dict[id] = next_id
+                embedding_table.append(vec)
+                next_id += 1
+            song_list.append(id_dict[id])
+        
+
+    #train_test split
+    inputs = tf.tensor(inputs)
+    labels = tf.tensor(labels)
+
+    seed = np.random.randint(1,1000)
+    inputs = tf.random.shuffle(data, seed)
+    labels = tf.random.shuffle(data, seed)
+
+    # HYPER-PARAM TEST PERCENTAGE
+    test_percentage = .2
+    split_point = test_percentage * labels.shape[0]
+
+    train_inputs = inputs[split_point:]
+    train_labels = labels[split_point:]
+
+    test_inputs = inputs[:split_point]
+    test_labels = labels[:split_point]
+
+    return train_inputs, train_labels, test_inputs, test_labels, embedding_table, id_dict
