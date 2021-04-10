7986efffd0b9704eb292dbc51c9d98e3aea8f7d7
Gus VanNewkirk <gusvannewkirk@Guss-MBP.fios-router.home>
2021-04-10 18:43:00 -0400

    big boy

--- Transformer.py
+++ Transformer.py
@@ -6,7 +6,7 @@ from preprocess import *
 
 hyper_params = {
      "batch_size": 32,
-     "num_epochs": 1,
+     "num_epochs": 65,
      "learning_rate": .001,
      "window_size": 3, #lol :(
      "embedding_size": 51,
