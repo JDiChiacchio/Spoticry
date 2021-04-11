0e01b9d5a2f0172921de1f46fcb05e47405ff935
JDiChiacchio <jacob_dichiacchio@brown.edu>
2021-04-11 00:11:08 -0400

    problem with database closing

--- acousticdb_merge.py
+++ acousticdb_merge.py
@@ -20,14 +20,14 @@ with sqlite3.connect(locstr + 'spoticry.db') as conn:
 
     # Compile a tempory full acoustic database table from components
     make_main_acoustic = '''
-      CREATE TABLE full_acoustic_brainz (
-        music_brainz_recording_id VARCHAR NOT NULL,
-        artist VARCHAR NOT NULL,
-        title VARCHAR NOT NULL,
-        cleaned_artist VARCHAR NOT NULL,
-        cleaned_title VARCHAR NOT NULL,
-        vec BLOB NOT NULL,
-        PRIMARY KEY (music_brainz_recording_id)
+    CREATE TABLE full_acoustic_brainz (
+      music_brainz_recording_id VARCHAR NOT NULL,
+      artist VARCHAR NOT NULL,
+      title VARCHAR NOT NULL,
+      cleaned_artist VARCHAR NOT NULL,
+      cleaned_title VARCHAR NOT NULL,
+      vec BLOB NOT NULL,
+      PRIMARY KEY (music_brainz_recording_id)
     );
     '''
     c.execute(make_main_acoustic)
@@ -35,42 +35,42 @@ with sqlite3.connect(locstr + 'spoticry.db') as conn:
     for file in glob.iglob(locstr + "acousticdb_components/*_acoustic.db"):
         c.execute('ATTACH DATABASE \'' + file + '\' AS \'component\'')
         c.execute('''
-          INSERT INTO full_acoustic_brainz
-          SELECT * FROM component.acoustic_brainz;
-      ''')
+        INSERT INTO full_acoustic_brainz
+        SELECT * FROM component.acoustic_brainz;
+    ''')
         conn.commit()
         c.execute('DETACH DATABASE \'component\'')
 
-  # Remove duplicate songs from full table
+    # Remove duplicate songs from full table
     remove_dup = '''
-      DELETE FROM full_acoustic_brainz
-      WHERE rowid NOT IN
-      (SELECT MIN(rowid)
-        FROM full_acoustic_brainz
-        GROUP BY cleaned_title, cleaned_artist);
-    '''
+    DELETE FROM full_acoustic_brainz
+    WHERE rowid NOT IN
+    (SELECT MIN(rowid)
+      FROM full_acoustic_brainz
+      GROUP BY cleaned_title, cleaned_artist);
+  '''
     c.execute(remove_dup)
 
     # JOIN on spoticry data
     make_transformer_table = '''
-      CREATE TABLE transformer (
-        song_id VARCHAR(18) NOT NULL,
-        user_id VARCHAR(40) NOT NULL,
-        vec BLOB NOT NULL
-      );
-    '''
+    CREATE TABLE transformer (
+      song_id VARCHAR(18) NOT NULL,
+      user_id VARCHAR(40) NOT NULL,
+      vec BLOB NOT NULL
+    );
+  '''
     c.execute(make_transformer_table)
 
     merge = '''
-      INSERT INTO transformer
-      SELECT b.song_id, b.user_id, add_sentiment(vec, sentiment)
-      FROM full_acoustic_brainz a
-      JOIN listening_songs b
-      ON b.cleaned_title = a.cleaned_title AND b.cleaned_artist = a.cleaned_artist
-      JOIN Sentiments c
-      ON b.song_id = c.song_id;
-    '''
+    INSERT INTO transformer
+    SELECT b.song_id, b.user_id, add_sentiment(vec, sentiment)
+    FROM full_acoustic_brainz a
+    JOIN listening_songs b
+    ON b.cleaned_title = a.cleaned_title AND b.cleaned_artist = a.cleaned_artist
+    JOIN Sentiments c
+    ON b.song_id = c.song_id;
+  '''
     c.execute(merge)
     # Delete tempory table and close connection
     c.execute('DROP TABLE full_acoustic_brainz')
-    conn.close()
+conn.close()
